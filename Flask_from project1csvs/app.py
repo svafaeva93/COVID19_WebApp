f5a15ff51f666ea0e86ff8311d08f48e6ae2f555
abdyraman <abdyramanova.j@gmail.com>
2023-06-06 18:55:00 -0400

    Mongo Atlas connection

--- Flask_from project1csvs/app.py
+++ Flask_from project1csvs/app.py
@@ -1,34 +1,40 @@
 
-from flask import Flask, jsonify
-from pymongo import MongoClient
+# from flask import Flask, jsonify
+# from pymongo import MongoClient
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
 # Create an instance of MongoClient
-mongo = MongoClient(port=27017)
-db = mongo.COVID19_pandemic
+from pymongo import MongoClient
+
+# Assign your connection string to a variable
+connection_string = "mongodb+srv://abdyramanovaj:covid19@cluster0.mymgc5e.mongodb.net/"
+
+# Create an instance of MongoClient using the connection string
+client = MongoClient(connection_string)
 
 # Collection name 
-COVID19_data = db['COVID19_data']
+COVID19_data = client.dataset_1
 
-@app.route('/')
-def index():
-    return 'Hello, World!'
 
-# Define API route
-@app.route('/data', methods=['GET'])
-def get_data():
-    data = []
-    query = {'Province': 'ON'}
-    results = COVID19_data.find(query)
-    for result in results:
-        data.append({
-            'id': str(result['_id']),
-            'Province': result['Province'],
-            'Cases': result['Cases']
-        })
+# @app.route('/')
+# def index():
+#     return 'Hello, World!'
 
-    return jsonify(data)
+# # Define API route
+# @app.route('/data', methods=['GET'])
+# def get_data():
+#     data = []
+#     query = {'Province': 'ON'}
+#     results = COVID19_data.find(query)
+#     for result in results:
+#         data.append({
+#             'id': str(result['_id']),
+#             'Province': result['Province'],
+#             'Cases': result['Cases']
+#         })
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#     return jsonify(data)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
