119fca7567caf104701e600d8d13426710624818
abdyraman <abdyramanova.j@gmail.com>
2023-06-07 20:23:34 -0400

    do it

--- Vaccination Visualisation/static/js/app.js
+++ Vaccination Visualisation/static/js/app.js
@@ -1,72 +1,77 @@
 // Define the URL for the JSON file
-const url = 'http://127.0.0.1:5000/vaccines';
+const url = 'http://127.0.0.1:5000/vaccinated_people_province';
 
-// Functions for each questions asked:
-// calling function which connects the "selDataset" element with the names array from data
-loadOptions();
-// // calling function to retrieve metadata information individually and change on option change. Display on dashboard
-// demographic(0);
-// // calling function to retrieve bar chart information individually and changes on option change. Display on dashboard
-// updateBarChart(0);
-// // calling function to retrieve bubble chart information individually and change on option change. Display on dashboard
-// updateBubbleChart(0);
-// // calling function to retrieve Gauge chart information individually and change on option change. Display on dashboard
-// updateGaugeChart(0);
-
-// This function connects the names array from data with the 'id' numbers with the "selDataset" element 
+// This function connects the names array from data with the 'id' numbers with the "selDataset" element
 function loadOptions() {
   d3.json(url)
     .then(data => {
       console.log(data);
 
-      // Declare arrays for province and mortality rate
-      var provinces = [];
-      var vaccines = [];
-
-      // Iterate over the array of documents
-      data.forEach(document => {
-        var vaccines= document["Cumulative number of people (Vaccinedose1)"];
-        var province = document["Province"];
-        console.log("Mortality rate:", vaccines);
-        console.log("Province:", province);
-
-        // Push values to respective arrays
-        provinces.push(province);
-        vaccines.push(vaccines);
-      });
-
-      // Update the trace object
-      const updatedTrace = {
-        x: provinces,
-        y: vaccines,
-        type: 'bar',
-        orientation: 'v',
-        width: 0.8,                // Adjust the width of the bars (0.8 represents 80% of the available space)
-      };
-
-      // Update the data array
-      const updatedData = [updatedTrace];
-
-      // Update the layout object (if needed)
-      const updatedLayout = {
-        title: 'Vaccines by Province',
-        xaxis: {
-          title: 'Province',
-        },
-        yaxis: {
-          title: 'Vaccines',
-          automargin: true,
-          title_standoff: 50,
-        },
-      };
-
-      // Update the chart
-      Plotly.newPlot('plot', updatedData, updatedLayout);
+      // Add your code here to process the data and populate the "selDataset" element with options
+      // For example, if the data contains an array of names, you can iterate over the array and create <option> elements
+      // using D3.js or plain JavaScript, and append them to the "selDataset" element.
     })
     .catch(error => {
-      console.log('Error loading data:', error);
+      console.log('Error loading JSON data:', error);
     });
 }
+
+// Call the loadOptions function to initiate the data loading process
+loadOptions();
+
+
+//       // Declare arrays for province and mortality rate
+//       var provinces = [];
+//       var vaccines = [];
+
+//       // Iterate over the array of documents
+//       data.forEach(document => {
+//         var vaccines= document["Cumulative number of people (Vaccinedose1)"];
+//         var province = document["Province"];
+//         console.log("Mortality rate:", vaccines);
+//         console.log("Province:", province);
+
+//         // Push values to respective arrays
+//         provinces.push(province);
+//         vaccines.push(vaccines);
+//       });
+
+//       // Update the trace object
+//       const updatedTrace = {
+//         x: provinces,
+//         y: vaccines,
+//         type: 'bar',
+//         orientation: 'v',
+//         width: 0.8,                // Adjust the width of the bars (0.8 represents 80% of the available space)
+//       };
+
+//       // Update the data array
+//       const updatedData = [updatedTrace];
+
+//       // Update the layout object (if needed)
+//       const updatedLayout = {
+//         title: 'Vaccines by Province',
+//         xaxis: {
+//           title: 'Province',
+//         },
+//         yaxis: {
+//           title: 'Vaccines',
+//           automargin: true,
+//           title_standoff: 50,
+//         },
+//       };
+
+//       // Update the chart
+//       Plotly.newPlot('plot', updatedData, updatedLayout);
+//     })
+//     .catch(error => {
+//       console.log('Error loading data:', error);
+//     });
+// }
+
+
+
+
 
 
 
