c1d7e2adfbeb98d9a23eea8d577e1d5536d993dd
samitaahmed <samitaahmed@gmail.com>
2023-06-09 17:10:21 -0400

    assign colors to prov

--- heatmap/static/js/logic4.js
+++ heatmap/static/js/logic4.js
@@ -19,15 +19,78 @@ function chooseColor(name) {
   else if (name == "British Columbia") return "orange";
   else if (name == "Saskatchewan") return "green";
   else if (name == "Manitoba") return "purple";
+  else if (name == "Quebec") return "blue";
+  else if (name == "New Brunswick") return "cyan";
+  else if (name == "Nova Scotia") return "brown";
+  else if (name == "Newfounland and Labrador") return "aquamarine";
+  else if (name == "Prince Edward Island") return "coral";
+  else if (name == "Northwest Territories") return "sienna";
+  else if (name == "Yukon") return "orchid";
+  else if (name == "Nunavut") return "lime";
   else return "black";
 }
 
+const provinceData = [
+  {
+    "_id": "Ontario",
+    "cum_confirmed_cases": 436487621
+  },
+  {
+    "_id": "Quebec",
+    "cum_confirmed_cases": 396869739
+  },
+  {
+    "_id": "Alberta",
+    "cum_confirmed_cases": 312265701
+  },
+  {
+    "_id": "British Columbia",
+    "cum_confirmed_cases": 197692349
+  },
+  {
+    "_id": "Manitoba",
+    "cum_confirmed_cases": 82397753
+  },
+  {
+    "_id": "Saskatchewan",
+    "cum_confirmed_cases": 50901860
+  },
+  {
+    "_id": "New Brunswick",
+    "cum_confirmed_cases": 19808333
+  },
+  {
+    "_id": "Nova Scotia",
+    "cum_confirmed_cases": 17072397
+  },
+  {
+    "_id": "Newfoundland and Labrador",
+    "cum_confirmed_cases": 13019523
+  },
+  {
+    "_id": "Prince Edward Island",
+    "cum_confirmed_cases": 11016190
+  },
+  {
+    "_id": "Northwest Territories",
+    "cum_confirmed_cases": 1670097
+  },
+  {
+    "_id": "Yukon",
+    "cum_confirmed_cases": 1643006
+  },
+  {
+    "_id": "Nunavut",
+    "cum_confirmed_cases": 431216
+  }
+];
 // Getting our GeoJSON data
 d3.json(link).then(function(data) {
   // Creating a GeoJSON layer with the retrieved data
   L.geoJson(data, {
     // Styling each feature (in this case, a neighborhood)
     style: function(feature) {
+      const province = provinceData.find(p => p._id === feature.properties.name);
       return {
         color: "white",
         // Call the chooseColor() function to decide which color to color our neighborhood. (The color is based on the borough.)
@@ -38,6 +101,11 @@ d3.json(link).then(function(data) {
     },
     // This is called on each feature.
     onEachFeature: function(feature, layer) {
+      const province = provinceData.find(p => p._id === feature.properties.name);
+      if (province) {
+        // Giving each feature a popup with information that's relevant to it
+        layer.bindPopup("<h1>" + feature.properties.name + "</h1> <hr> <h2>Confirmed Cases: " + province.cum_confirmed_cases + "</h2>");
+      }
       // Set the mouse events to change the map styling.
       layer.on({
         // When a user's mouse cursor touches a map feature, the mouseover event calls this function, which makes that feature's opacity change to 90% so that it stands out.
