4ff5d8ada3a13fccc6c4127911dfe6f41e7942f2
abdyraman <abdyramanova.j@gmail.com>
2023-06-09 13:30:24 -0400

    latest updates

--- Vaccination Visualisation/static/js/app.js
+++ Vaccination Visualisation/static/js/app.js
@@ -5,6 +5,7 @@ const url2='http://127.0.0.1:5000/vaccines'
 // Calling the functions
 vaccinations();
 vaccines();
+fetchVaccinatedData();
 
 function vaccinations() {
   d3.json(url)
@@ -25,7 +26,7 @@ function vaccinations() {
       });
       // Update the trace object
       const updatedTrace = {
-        y: provinces,
+        y: provinces.reverse(),
         x: vaccinations.reverse(),
         type: 'bar',
         orientation: 'h',
@@ -37,19 +38,25 @@ function vaccinations() {
       // Update the data array
       const updatedData = [updatedTrace];
       // Update the layout object
-      const updatedLayout = {
-        title: 'Vaccinated People by Province',
-        xaxis: {
-          title: 'Province'
-        },
-        yaxis: {
-          title: 'Vaccinated People',
-          automargin: true,
-          title_standoff: 50
-        }
-      };
-      // Update the chart
-      Plotly.newPlot('plot', updatedData, updatedLayout);
+// Update the layout object
+const updatedLayout = {
+  title: 'Vaccinated People by Province',
+  xaxis: {
+    title: 'Vaccinated People',
+    automargin: true,
+    title_standoff: 50
+  },
+  yaxis: {
+    title: 'Province',
+    automargin: true,
+    title_standoff: 50
+  },
+  width: 700, // Adjust the width of the hbar chart here
+  height: 500 // Adjust the height of the hbar chart here
+};
+// Update the chart
+Plotly.newPlot('plot', updatedData, updatedLayout);
+
     })
     .catch(error => {
       console.log('Error loading data:', error);
@@ -113,7 +120,8 @@ function updateVaccineNumbers(selectedProvince) {
 
   demographicInfo.append("h5").text("Vaccine Numbers in " + selectedProvince + ":");
   filteredData.forEach(data => {
-    demographicInfo.append("p").text(data.date + ": " + data.vaccine);
+    var formattedVaccine = data.vaccine.toLocaleString(); // Format the vaccinated number with commas or periods
+    demographicInfo.append("p").text(data.date + ": " + formattedVaccine);
   });
 }
 
@@ -123,3 +131,78 @@ d3.select("#provinceDropdown").on("change", function () {
   updateVaccineNumbers(selectedProvince);
 });
 
+// Declare global variables for vaccinatedData
+var vaccinatedProvinces = [];
+var vaccinatedNumbers = [];
+var vaccinatedDates = [];
+
+function fetchVaccinatedData() {
+  d3.json(url2)
+    .then(data => {
+      console.log(data);
+
+      // Clear previous content
+      d3.select("#plot3").html("");
+
+      // Iterate over the array of documents
+      data.forEach(document => {
+        var vaccinatedNumber = document["Cumulative number of people (Vaccinedose1)"];
+        var vaccinatedProvince = document["Province"];
+        var vaccinatedDate = new Date(document["Date"]).toLocaleDateString('en-US', {
+          month: 'short',
+          day: 'numeric',
+          year: 'numeric'
+        });
+        console.log("Vaccinated Number:", vaccinatedNumber);
+        console.log("Date:", vaccinatedDate);
+        console.log("Province:", vaccinatedProvince);
+
+        // Push values to respective arrays
+        vaccinatedProvinces.push(vaccinatedProvince);
+        vaccinatedNumbers.push(vaccinatedNumber);
+        vaccinatedDates.push(vaccinatedDate);
+      });
+
+      // Sort data by date
+      var sortedData = vaccinatedDates.map((date, index) => ({ date: new Date(date), number: vaccinatedNumbers[index] }));
+      sortedData.sort((a, b) => a.date - b.date);
+
+      // Extract sorted dates and vaccinated numbers
+      var sortedDates = sortedData.map(entry => entry.date);
+      var sortedVaccinatedNumbers = sortedData.map(entry => entry.number);
+
+      // Create the bar trace
+      var trace = {
+        x: sortedDates,
+        y: sortedVaccinatedNumbers,
+        type: 'bar',
+        marker: {
+          color: 'rgba(0, 128, 0, 0.8)'
+        }
+      };
+
+      // Create the data array
+      var data = [trace];
+
+// Create the layout
+var layout = {
+  title: 'Vaccinated People Over Time',
+  xaxis: {
+    title: 'Date',
+    tickformat: '%b %d, %Y' // Format the date tick labels
+  },
+  yaxis: {
+    title: 'Vaccinated People',
+    tickformat: ',.0f' // Format the y-axis tick labels with commas as thousands separators
+  },
+  width: 600, // Set the desired width in pixels
+  height: 600 // Set the desired height in pixels
+};
+
+// Plot the chart
+Plotly.newPlot('plot3', data, layout);
+})
+.catch(error => {
+  console.log('Error loading data:', error);
+});
+}
